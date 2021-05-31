b3e38de80959475c38bd9b336ca097b08094d991
Ishanuj <ishanuj99@gmail.com>
2021-05-31 13:54:19 +0530

    Second commit

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ if(process.env.NODE_ENV !== 'production')
 {
     require('dotenv').config()
 }
-const express = require('express')
-const app = express()
+const http = require('http')
+const app = require ('./app')
+const port = process.env.PORT || 3000
+
 const expressLayouts= require('express-ejs-layouts')
+const server =http.createServer(app)
 
-const indexRouter = require('./routes/index')
-
+/*
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -21,7 +23,7 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error =>console.error(error))
 db.once('open', () =>console.log('Connected to Mongoose'))
+*/
+const indexRouter = require('./routes/product')
 
-app.use('/',indexRouter)
-
-app.listen(process.env.PORT || 3000)
+server.listen(port)
